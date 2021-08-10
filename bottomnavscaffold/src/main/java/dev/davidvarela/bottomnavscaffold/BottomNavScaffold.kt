be134986fa9053b61cdbeea8d57d6bef5bb00f33
David Varela <coding@davidvarela.dev>
2021-08-10 11:15:09 +0200

    doc: add public documentation, and license on every single code file

--- bottomnavscaffold/src/main/java/dev/davidvarela/bottomnavscaffold/BottomNavScaffold.kt
+++ bottomnavscaffold/src/main/java/dev/davidvarela/bottomnavscaffold/BottomNavScaffold.kt
@@ -1,13 +1,33 @@
+/*
+   Copyright 2021 David González Varela
+
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+       http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+ */
 package dev.davidvarela.bottomnavscaffold
 
 import androidx.compose.foundation.layout.ColumnScope
+import androidx.compose.material.BackdropScaffold
+import androidx.compose.material.BottomSheetScaffold
 import androidx.compose.material.DrawerDefaults
 import androidx.compose.material.FabPosition
+import androidx.compose.material.FloatingActionButton
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Scaffold
 import androidx.compose.material.ScaffoldState
+import androidx.compose.material.Snackbar
 import androidx.compose.material.SnackbarHost
 import androidx.compose.material.SnackbarHostState
+import androidx.compose.material.TopAppBar
 import androidx.compose.material.contentColorFor
 import androidx.compose.material.rememberScaffoldState
 import androidx.compose.runtime.Composable
@@ -15,8 +35,59 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.Shape
 import androidx.compose.ui.unit.Dp
+import androidx.navigation.NavHost
 import androidx.navigation.NavHostController
+import androidx.navigation.Navigation
 
+/**
+ * <a href="https://material.io/design/layout/understanding-layout.html" class="external" target="_blank">Material Design layout</a>.
+ *
+ * BottomNavScaffold implements the basic material design visual layout structure with a bottom
+ * navigation bar.
+ *
+ * This component provides API to put together several material components to construct your
+ * screen, by ensuring proper layout strategy for them and collecting necessary data so these
+ * components will work together correctly.
+ *
+ * For similar components that implement different layout structures, see [BackdropScaffold],
+ * which uses a backdrop as the centerpiece of the screen, and [BottomSheetScaffold], which uses
+ * a persistent bottom sheet as the centerpiece of the screen.
+ *
+ * @param navHostController [NavHostController] obtained from a navigation host via
+ * [NavHost.navController] or by using one of the utility methods on the [Navigation] class.
+ * @param screens list of [BottomNavScreen], destinations of the bottom nav bar
+ * @param modifier optional Modifier for the root of the [Scaffold]
+ * @param startDestination optional String with the [BottomNavScreen.route] of tne initial
+ * destination. The first destination of the list is the initial destination by default.
+ * @param scaffoldState state of this scaffold widget. It contains the state of the screen, e.g.
+ * variables to provide manual control over the drawer behavior, sizes of components, etc
+ * @param topBar top app bar of the screen. Consider using [TopAppBar].
+ * @param snackbarHost component to host [Snackbar]s that are pushed to be shown via
+ * [SnackbarHostState.showSnackbar]. Usually it's a [SnackbarHost]
+ * @param floatingActionButton Main action button of your screen. Consider using
+ * [FloatingActionButton] for this slot.
+ * @param floatingActionButtonPosition position of the FAB on the screen. See [FabPosition] for
+ * possible options available.
+ * @param isFloatingActionButtonDocked whether [floatingActionButton] should overlap with
+ * bottomBar for half a height.
+ * @param drawerContent content of the Drawer sheet that can be pulled from the left side (right
+ * for RTL).
+ * @param drawerGesturesEnabled whether or not drawer (if set) can be interacted with via gestures
+ * @param drawerShape shape of the drawer sheet (if set)
+ * @param drawerElevation drawer sheet elevation. This controls the size of the shadow
+ * below the drawer sheet (if set)
+ * @param drawerBackgroundColor background color to be used for the drawer sheet
+ * @param drawerContentColor color of the content to use inside the drawer sheet. Defaults to
+ * either the matching content color for [drawerBackgroundColor], or, if it is not a color from
+ * the theme, this will keep the same value set above this Surface.
+ * @param drawerScrimColor color of the scrim that obscures content when the drawer is open
+ * @param backgroundColor background of the scaffold body
+ * @param contentColor color of the content in scaffold body. Defaults to either the matching
+ * content color for [backgroundColor], or, if it is not a color from the theme, this will keep
+ * the same value set above this Surface.
+ */
+
+@Suppress("unused")
 @Composable
 fun BottomNavScaffold(
     navHostController: NavHostController,
--- bottomnavscaffold/src/main/java/dev/davidvarela/bottomnavscaffold/BottomNavScreen.kt
+++ bottomnavscaffold/src/main/java/dev/davidvarela/bottomnavscaffold/BottomNavScreen.kt
@@ -1,3 +1,18 @@
+/*
+   Copyright 2021 David González Varela
+
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+       http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+ */
 package dev.davidvarela.bottomnavscaffold
 
 import androidx.annotation.DrawableRes
@@ -5,6 +20,22 @@ import androidx.annotation.StringRes
 import androidx.compose.runtime.Composable
 import androidx.navigation.NavBackStackEntry
 
+/**
+ * Representation of an option of the bottom navigation bar, a destination of the app.
+ *
+ * @param route String that identifies each destination, used for navigating
+ * @param titleResourceId Resource identifier of the title of the destination in the bottom bar,
+ * R.string.section_title
+ * @param iconResourceId Resource identifier of the icon of the destination in the bottom bar,
+ * R.drawable.section_icon
+ * @param iconContentDescription String with the content description {@code iconResourceId}, for
+ * accessibility. Null by default
+ * @param content Composable to draw in the destination
+ * @see <a href="https://material.io/components/bottom-navigation/android">bottom navigation in material.io</a>
+ * @see <a href="https://developer.android.com/jetpack/compose/navigation#bottom-nav">Navigating with compose: integration with the bottom nav bar</a>
+ * @see <a href="https://developer.android.com/codelabs/jetpack-compose-navigation">Jetpack Compose Navigation codelab</a>
+ *
+ */
 data class BottomNavScreen(
     val route: String,
     @StringRes val titleResourceId: Int,
